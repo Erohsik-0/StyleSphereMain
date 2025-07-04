4f2f6c5481ecc16db4153517da9e0bfc223d3757
Kishore Sundaramurthi <kishorekishore9725@gmail.com>
2025-07-04 11:47:03 +0530

    Initial-commit

---
+++ StyleSphere.Service/IVerificationStore.cs
@@ -0,0 +1,10 @@
+﻿namespace StyleSphere.Service
+{
+    public interface IVerificationStore
+    {
+        void StoreCode(string key, string code);
+        string? GetCode(string key);
+        void RemoveCode(string key);
+
+    }
+}
---
+++ StyleSphere.Service/InMemoryVerificationStore.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace StyleSphere.Service
+{
+    public class InMemoryVerificationStore : IVerificationStore
+    {
+
+        private readonly Dictionary<string, string> _store = new();
+
+        public void StoreCode(string key, string code) => _store[key] = code;
+
+        public string? GetCode(string key) => _store.TryGetValue(key, out var code) ? code : null;
+
+        public void RemoveCode(string key)
+        {
+            if (_store.ContainsKey(key))
+            {
+                _store.Remove(key);
+            }
+        }
+
+    }
+}
